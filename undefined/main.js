c4549ab64c08d4d3dff1b320e1a8a8b67d3a37b6
raxit-Cybersync <raxit@cybersync-technologies.com>
2026-02-25 12:46:52 +0530

    learnt about undefined and not defined

---
+++ undefined/main.js
@@ -0,0 +1,17 @@
+// console.log(a);
+// var a = 7;
+// console.log(a);
+
+
+
+var a;
+console.log(a);
+a = 10;
+console.log(a);
+a = "Hello world";
+console.log(a);
+hello ="adf"
+console.log(typeof hello);
+
+
+
