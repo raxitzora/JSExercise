0cf04ae17f8a4fca0fb8066f1ea13d68a086db2d
raxitzora <raxitzora@email.com>
2026-03-10 23:51:36 +0530

    let const var

---
+++ AksaySaini/variables.js
@@ -0,0 +1,56 @@
+// //
+// var a = 10;
+// console.log(a);
+
+
+
+// if (true) {
+//   var x = 10;
+// }
+
+// console.log(x);
+
+
+
+// var a = 10;
+// var a = 20;
+
+// console.log(a);
+
+
+
+// console.log(a);
+// var a = 5;
+
+
+// var a = 10;
+
+// console.log(window.a);
+
+// for(var i = 0;i<=3;i++){
+//     setTimeout(()=>console.log(i),100)
+// }
+
+// for (var i = 0; i < 3; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// let x = 10;
+
+// {
+//   let x = 20;
+//   console.log(x);
+// }
+
+// console.log(x);
+
+// let user = "Rax";
+// function test(){
+//     let user = "Admin";
+//     console.log(user);
+// }
+// test()
+// console.log(user);
+
