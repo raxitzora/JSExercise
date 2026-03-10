3cc0fd33a49321b233fc67f049bea05352d8a0bc
raxitzora <raxitzora@email.com>
2026-03-10 23:38:53 +0530

    hoisting completed

--- AksaySaini/Hoisting.js
+++ AksaySaini/Hoisting.js
@@ -1,2 +1,86 @@
-console.log(a);
-var a = 10;
+// var a;
+// console.log(a);
+// a = 19
+
+
+
+// greet()
+
+// function greet(){
+//     console.log("Hello");
+// }
+
+
+// console.log(a);
+// let a = 10;
+
+// greet();
+
+// var greet  = function(){
+// console.log("Hello");
+
+// }
+
+
+
+
+
+// console.log(a);
+// var a= 10;
+//-> undefined error
+
+
+
+// greet()
+// function greet(){
+//     console.log("Hello");
+// }
+//-> print Hello
+
+
+
+// greet();
+// var greet = function(){
+//     console.log("Hello");
+// }
+//->greet is not function error (Type Error)
+
+
+// var greet;
+// greet();
+// greet = function(){
+//     console.log("Hello");
+// }
+//->greet is not function 
+
+
+// console.log(a);
+// let a = 5;
+//-> RefernceError
+
+// console.log(a);
+// const a= 10;
+//-> RefernceError
+
+
+// var x = 1;
+
+// function test(){
+//     console.log(x);
+//     var x= 2;
+// }
+// test()
+//-> undefined error
+
+
+
+// let x= 1;
+// function test(){
+//     console.log(x);
+//     let x = 2;
+// }
+// test()
+//-> reference error
+
+
+
