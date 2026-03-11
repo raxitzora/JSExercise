a474ef9a3ea8c8b8c8dc045b058975319b03075f
raxitzora <raxitzora@email.com>
2026-03-11 22:14:36 +0530

    arrays

---
+++ arrays/ex.js
@@ -0,0 +1,16 @@
+//array
+const arr = [1,2,3,4,5]
+
+const my = new Array("rax","as","sd")
+// console.log(my);
+
+// console.log(my.length);
+
+// arr.pop()
+// console.log(arr);
+
+
+
+
+
+
---
+++ arrays/ex2.js
@@ -0,0 +1,7 @@
+const mar = ["spiderman","ironman","captainamerica"]
+
+const dc = ["superman","batman","flash"]
+
+const all = [...mar]
+console.log(all);
+
