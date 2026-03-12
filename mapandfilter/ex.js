20d476b73c72028fcc60e652095a6638137d0988
raxitzora <raxitzora@gmail.com>
2026-03-12 23:44:19 +0530

    maps

---
+++ mapandfilter/ex.js
@@ -0,0 +1,12 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const newnum = numbers.filter((num)=>num>5)
+console.log(newnum);
+
+
+
+const ct = coding.filter((item)=>item.length>"4")
+console.log(ct);
