25e05ca8fdca536f0d339e7215a4c0314528d24f
raxitzora <raxitzora@gmail.com>
2026-03-12 23:35:34 +0530

    loops

--- loops/ex.js
+++ loops/ex.js
@@ -2,10 +2,24 @@ const arr = [1,2,3,4,5]
 const names = ["raxit","alice","bob"]
 
 for(const array of arr){
-    console.log(array);
+    // console.log(array);
 }
 
 for(const name of names){
-    console.log(name);
+    // console.log(name);
+    
+}
+
+const myobject = {
+    "name1":"raxit",
+    "name2":"Harivandana",
+    "name3":"alice"
+}
+
+
+for (const key in myobject) {
+    console.log(myobject[key]);
+    
+    
     
 }
