2a56d43a340aa830010d29f4e9c2be4442778723
raxitzora <raxitzora@gmail.com>
2026-03-16 02:07:18 +0530

    map filter reduce

--- e.js
+++ e.js
@@ -1,10 +1,88 @@
-function makePizza(callback) {
-  console.log("Pizza ban raha hai...");
-  callback(); 
-}
+// function cookFood(callback){
+//     console.log("Khana ban raha hai");
+//     callback();
+// }
 
-function ready() {
-  console.log("Pizza ready hai!");
-}
 
-makePizza(ready);
+// function readyFood(){
+//     console.log("Khana ban chuka hai");
+// }
+
+// cookFood(readyFood)
+
+// function cookingpizza(done){
+//     console.log("Pizza is being cooked");
+//     done()
+// }
+
+// function pizzaready(){
+//     console.log("Pizza ready");
+// }
+
+// cookingpizza(pizzaready)
+
+// const number  = [1,2,3,4,5,6]
+
+// const double1 = number.map(function(n){
+//     return n*2;
+// })
+
+// const double2 = number.map((num)=>{
+//     return num *2
+// })
+// console.log(double1);
+// console.log(double2);
+
+// const users = [
+//   {name:"Raxit"},
+//   {name:"Rahul"},
+//   {name:"Amit"}
+// ]
+
+
+// const names = users.map(function(n){
+//     return n.name
+// })
+// console.log(names);
+
+
+// const numbers = [1,2,3,4,5,6]
+
+// const filtered = numbers.filter((i)=>{
+//     return i<5
+// })
+// console.log(filtered);
+
+// const users = [
+//  {name:"A", active:true},
+//  {name:"B", active:false},
+//  {name:"C", active:true}
+// ]
+
+// const ActiveUsers = users.filter((user)=>{
+//     return user.active
+// })
+// console.log(ActiveUsers);
+
+
+// const numbers = [1,2,3,4]
+
+
+// const sum = numbers.reduce((acc,n)=>{
+//     return acc + n
+// },0)
+// console.log(sum);
+
+// const cart = [
+//  {price:100},
+//  {price:200},
+//  {price:300}
+// ]
+
+// const total = cart.reduce((sum,item)=>{
+//     return sum + item.price
+// },0)
+// console.log(total);
+
+
+
